ec53b2b6cfad36b816259ebb9feb9f218d527a36
Michael <michael.kislev@spectory.com>
2017-10-31 11:56:21 +0200

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/get_users', to: 'users#get_users'
+  get 'get_users', to: 'users#get_users'
 
 end
