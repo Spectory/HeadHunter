3487d98da5b4b2b67b33db288b142e2b45a36fa1
zvikae <zvika.e@gmail.com>
2017-11-01 23:02:13 +0200

    get candidates data

--- app/controllers/candidate_controller.rb
+++ app/controllers/candidate_controller.rb
@@ -1,5 +1,6 @@
 class CandidateController < ApplicationController
   def get_candidates
+    params_formatted = JSON.parse(params[:search])
     candidates = Candidate.all
     render json: candidates, status: 200
   end
